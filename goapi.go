807c2ca19d516e493342538631b8a3e5cb9124e9
pavanleton <pavankumarkpkp@gmail.com>
2023-09-23 23:40:39 +0530

    APIs to add and get all books

--- goapi.go
+++ goapi.go
@@ -1,21 +1,56 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
+	"os"
 )
 
-func addBook() {
-
+type Book struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
 }
 
-func updateBookById() {
+var books = make(map[string]Book)
 
+func addBook(w http.ResponseWriter, req *http.Request) {
+	var book Book
+	err := json.NewDecoder(req.Body).Decode(&book)
+	fmt.Println(book)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	books[book.ID] = book
+
+	fmt.Fprintf(w, "%v", book)
 }
 
-func deleteBookById() {
+func getAllBooks(w http.ResponseWriter, req *http.Request) {
+	var allBook []Book
 
+	for _, value := range books {
+		allBook = append(allBook, value)
+	}
+
+	fmt.Fprintf(w, "%v", allBook)
 }
 
 func main() {
-	http.ListenAndServe(":4000", nil)
+	http.HandleFunc("/books/", getAllBooks)
+	http.HandleFunc("/books/add", addBook)
+
+	err := http.ListenAndServe(":3333", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
