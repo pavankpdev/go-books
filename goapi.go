70738b7f696cb8c25eb52aa274b12c5a03042c3c
pavanleton <pavankumarkpkp@gmail.com>
2023-09-23 21:50:47 +0530

    first commit

---
+++ goapi.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+)
+
+func addBook() {
+
+}
+
+func updateBookById() {
+
+}
+
+func deleteBookById() {
+
+}
+
+func main() {
+	http.ListenAndServe(":4000", nil)
+}
