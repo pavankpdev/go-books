b45a6c6fa2514cced461a71eec6cbf148c7605ac
pavanleton <pavankumarkpkp@gmail.com>
2023-09-24 12:59:28 +0530

    add update and delete API

--- goapi.go
+++ goapi.go
@@ -19,7 +19,6 @@ var books = make(map[string]Book)
 func addBook(w http.ResponseWriter, req *http.Request) {
 	var book Book
 	err := json.NewDecoder(req.Body).Decode(&book)
-	fmt.Println(book)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -41,9 +40,44 @@ func getAllBooks(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "%v", allBook)
 }
 
+func updateBookById(w http.ResponseWriter, req *http.Request) {
+	var book Book
+	err := json.NewDecoder(req.Body).Decode(&book)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	books[book.ID] = book
+	fmt.Fprintf(w, "%v", book)
+}
+
+func deleteBookById(w http.ResponseWriter, req *http.Request) {
+	var inputData map[string]string
+	err := json.NewDecoder(req.Body).Decode(&inputData)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id, ok := inputData["id"]
+
+	if !ok {
+		http.Error(w, "Missing 'id' field in JSON data", http.StatusBadRequest)
+		return
+	}
+
+	delete(books, id)
+	fmt.Fprintf(w, "book with the id:%s is deleted", id)
+}
+
 func main() {
-	http.HandleFunc("/books/", getAllBooks)
+	http.HandleFunc("/books/all", getAllBooks)
 	http.HandleFunc("/books/add", addBook)
+	http.HandleFunc("/books/update", updateBookById)
+	http.HandleFunc("/books/delete", deleteBookById)
 
 	err := http.ListenAndServe(":3333", nil)
 
